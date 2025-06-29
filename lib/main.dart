e1c516225a790ed40dcbb899795de0aad6c349a6
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 <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>
2025-06-29 10:11:02 -0300

    structure update

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hephzibah/app/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-          title: Text('Starter'),
-        ),
-        body: Center(child: Text('App Starter')),
-      ),
-    );
-  }
+  runApp(const ProviderScope(child: MyApp()));
 }
